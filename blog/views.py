2a3148b29f36bc87df91cd5af042455f041cf933
cute3954 <cute3954@naver.com>
2019-11-20 22:58:13 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -25,3 +25,21 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form':form})
+# 書き込み修正のビュー
+def post_edit(request, pk):
+    # 修正したい書き込みのPostモデルのinstanceを呼び出す
+    # pkで修正する書き込みを指定する
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        # 取得した書き込みのデータでフォームを作る
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        # 修正したデータを保存
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form':form})
