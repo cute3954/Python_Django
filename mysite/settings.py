0c41b3d61212adf1f7b901f1942de23fba8a8f60
cute3954 <cute3954@naver.com>
2019-12-16 22:50:10 +0900

    Modified settings.py for security issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,15 +16,14 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'al&mmflj4bwyccw$x@!*h4ah_7q1st_x_#u)2@ftpkg@1w!y!5'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'Dal&mmflj4bwyccw$x@!*h4ah_7q1st_x_#u)2@ftpkg@1w!y!5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.herokuapp.com']
 
